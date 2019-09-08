b5ed65290e3859916465f7b46c207867584b99de
Luiz Rosa <lcarlosrjr@gmail.com>
2019-09-08 13:39:03 -0300

    apiv2-deploy1

---
+++ src/main/java/com/treasy/apiv2/Apiv2Application.java
@@ -0,0 +1,25 @@
+package com.treasy.apiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Apiv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apiv2Application.class, args);
+	}
+
+}
+
+@RestController
+class HelloController{
+	
+	@GetMapping("/")
+	String hello() {
+		return "Olá mundo complicado...";
+				
+	}
+}
