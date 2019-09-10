73df8c5c9b211f592dabe93dd11339f065525381
Luiz Rosa <lcarlosrjr@gmail.com>
2019-09-09 23:30:34 -0300

    pesquisa por id

--- src/main/java/com/treasy/apiv2/repository/NodeRepository.java
+++ src/main/java/com/treasy/apiv2/repository/NodeRepository.java
@@ -1,6 +1,6 @@
 package com.treasy.apiv2.repository;
 
-import java.util.List;
+//import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +8,6 @@ import com.treasy.apiv2.model.Node;
 
 public interface NodeRepository extends JpaRepository<Node, Long> {
 	
-	List<Node> findbyparentid(long parentid);
+	//List<Node> findbyparentid(long parentid);
 
 }
